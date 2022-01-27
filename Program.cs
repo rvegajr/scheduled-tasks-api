70c422a2b5cf0e50c61809f99e60986bfe9161c3
Ricardo Vega <Ricardo.Vega@pxd.com>
2022-01-27 12:38:11 -0600

    Additonal endpoints

--- Program.cs
+++ Program.cs
@@ -20,12 +20,8 @@ builder.Services.AddAuthorization(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthentication();
 app.UseAuthorization();
